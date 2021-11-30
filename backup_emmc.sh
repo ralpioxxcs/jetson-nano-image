cf2e37093a900d32d3f445affa463e2e36697fc0
ralpioxxcs <tmfmfkal1123@naver.com>
2021-11-30 09:34:17 +0900

    backup script

--- backup_emmc.sh
+++ backup_emmc.sh
@@ -4,9 +4,10 @@
 # Author: ralpioxxcs
 #
 
-# @brief : Flash to jetson nano emmc board 
+# @brief : Backup to jetson nano emmc board 
 # @env :
-#   * JETSON_BUILD_DIR - build output directory
+#   * JETSON_BACKUP_IMG - filename of backup
+#   * JETSON_L4T_DIR    - L4T base directory ( ${JETSON_L4T_DIR}/Linux_for_Tegra )
 
 set -e
 
@@ -18,7 +19,7 @@ ENDCOLOR="\e[0m"
 
 main() {
     printf "${BOLDGREEN}- - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n${ENDCOLOR}"
-    printf "${BOLDGREEN}Flash eMMC image to jetson-nano-eMMC production board.\n${ENDCOLOR}"
+    printf "${BOLDGREEN}Backup eMMC image to jetson-nano-eMMC production board.\n${ENDCOLOR}"
     printf "${BOLDGREEN}- - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n${ENDCOLOR}"
 
     if [ "x$(whoami)" != "xroot" ]; then
@@ -29,14 +30,17 @@ main() {
     check_device
 
     # Check env variables 
-    if [ ! ${JETSON_BUILD_DIR} ]; then
+    if [ ! ${JETSON_L4T_DIR} ]; then
 	    printf "\e[31mL4T directory is not specified\e[0m\n"
         confirm_exit
     fi
+    if [ ! ${JETSON_BACKUP_IMG} ]; then
+        export JETSON_BACKUP_IMG=jetson_backup.img
+	    printf "\e[33mfilename is not specified,, use predefined (${JETSON_BACKUP_IMG})\e[0m\n"
+    fi
 
-    flashing
+    backup
 }
-
 
 function confirm_exit() {
 	read -n1 -p "Press any key to exit."
@@ -57,30 +61,28 @@ function check_device() {
   fi
 }
 
-function flashing() {
-  if [ ! "$(ls -A ${JETSON_BUILD_DIR})" ]; then
+function backup() {
+  if [ ! "$(ls -A ${JETSON_L4T_DIR})" ]; then
 	  printf "\e[31mNo L4T Directory found in \"${JETSON_ROOTFS_DIR}\"\e[0m\n"
     confirm_exit
   fi
 
-  local l4t_dir=${JETSON_BUILD_DIR}/Linux_for_Tegra
+  local l4t_dir=${JETSON_L4T_DIR}/Linux_for_Tegra
   pushd ${l4t_dir}
 
-  # Flashing
+  # Backup 
   local board=jetson-nano-emmc # device
   local partition=mmcblk0p1 # eMMC partition
-  sudo ./flash.sh ${board} ${partition}
+  ./flash -r -k APP -G ${JETSON_BACKUP_IMG} ${board} ${partition}
   if [ $? -eq 0 ]; then
-    printf "\e[32mFlashing is completed successfuly\e[0m\n"
+    printf "\e[32mSuccess to backup APP image\e[0m\n"
   else
-    printf "\e[31mFailed to flashing to device\e[0m\n"
+    printf "\e[31mFailed to backup APP image\e[0m\n"
   fi
 
   popd
 
   confirm_exit
 }
-
-
 
 main "$@"; exit
